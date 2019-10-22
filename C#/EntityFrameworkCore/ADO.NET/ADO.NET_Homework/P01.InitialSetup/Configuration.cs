a99b15f3ea5dcf83129c54f77de9cdabb21ac72a
KirilNS <kirilnstoikov@gmail.com>
2019-10-22 21:28:19 +0300

    Create Database

---
+++ C#/EntityFrameworkCore/ADO.NET/ADO.NET_Homework/P01.InitialSetup/Configuration.cs
@@ -0,0 +1,8 @@
+﻿
+namespace P01.InitialSetup
+{
+    public static class Configuration
+    {
+        public const string connectionString= "Server=DESKTOP-FB04SD9\\SQLEXPRESS;;Integrated Security=true";
+    }
+}
---
+++ C#/EntityFrameworkCore/ADO.NET/ADO.NET_Homework/P01.InitialSetup/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace P01.InitialSetup
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = Configuration.connectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            using (connection)
+            {
+                string cmdText = "CREATE DATABASE MinionsDB";
+
+                connection.Open();
+
+                NonQuery(connection, cmdText);
+            }
+        }
+
+        private static void NonQuery(SqlConnection connection, string cmdText)
+        {
+            using (SqlCommand command = new SqlCommand(cmdText, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
