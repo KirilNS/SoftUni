daabb7c7e94f13b286544a82175959d4ac200359
KirilNS <kirilnstoikov@gmail.com>
2019-10-23 17:38:44 +0300

    P.01 Done

--- C#/EntityFrameworkCore/ADO.NET/ADO.NET_Homework/P01.InitialSetup/Configuration.cs
+++ C#/EntityFrameworkCore/ADO.NET/ADO.NET_Homework/P01.InitialSetup/Configuration.cs
@@ -3,6 +3,6 @@ namespace P01.InitialSetup
 {
     public static class Configuration
     {
-        public const string connectionString= "Server=DESKTOP-FB04SD9\\SQLEXPRESS;;Integrated Security=true";
+        public const string connectionString= "Server=DESKTOP-FB04SD9\\SQLEXPRESS;DATABASE = MinionsDB;Integrated Security=true";
     }
 }
