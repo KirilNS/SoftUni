a129affaae4d4f09edd9d4ae7e49c9035891a718
KirilNS <kirilnstoikov@gmail.com>
2019-10-30 21:52:53 +0200

    P04.Done

---
+++ C#/EntityFrameworkCore/EntityFrameworkIntro/EF-Intro/SoftUni/StartUp.cs
@@ -0,0 +1,62 @@
+﻿using SoftUni.Data;
+using System.Linq;
+using System;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (SoftUniContext context=new SoftUniContext())
+            {
+                var result=GetEmployeesWithSalaryOver50000(context);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new { e.FirstName, e.Salary })
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.FirstName} - {emp.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary,
+                    e.EmployeeId
+                })
+                .OrderBy(e => e.EmployeeId).ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.FirstName} {emp.LastName} {emp.MiddleName} {emp.JobTitle} {emp.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
