610d3dd46bb3e97518a039911cc21bd0ab898e3f
KirilNS <kirilnstoikov@gmail.com>
2019-10-30 18:21:48 +0200

    All Done

--- C#/EntityFrameworkCore/MiniORM/MiniORM/DbContext.cs
+++ C#/EntityFrameworkCore/MiniORM/MiniORM/DbContext.cs
@@ -1,4 +1,69 @@
-﻿namespace MiniORM
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniORM
 {
-	// TODO: Create your DbContext class here.
+    public abstract class DbContext
+    {
+        private readonly DatabaseConnection connection;
+
+        private readonly Dictionary<Type, PropertyInfo> dbSetProperties;
+
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime)
+        };
+
+        public DbContext(string connectionString)
+        {
+            this.connection = new DatabaseConnection(connectionString);
+
+            this.dbSetProperties = this.DiscoverDbSets();
+
+            using (new ConnectionManager(connection))
+            {
+                this.InitializeDbSets();
+            }
+
+            this.MapAllRelations();
+        }
+
+        public void SaveChanges()
+        {
+            var dbSets = this.dbSetProperties
+                .Select(pi => pi.Value.GetValue(this))
+                .ToArray();
+
+            foreach (IEnumerable<object> dbset in dbSets)
+            {
+                var invalidEntities = dbset
+                    .Where(entity => !IsObjectValid(entity)).ToArray();
+
+                if (invalidEntities.Any())
+                {
+                    throw new InvalidOperationException($"{invalidEntities.Lenght} Invalid Entities found in {dbset.GetType().Name}!");
+                }
+            }
+
+            using (new ConnectionManager(connection))
+            {
+                using (var transaction=this.connection.StartTransaction())
+                {
+                    foreach (IEnumerable dbSet in dbSets)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
 }
