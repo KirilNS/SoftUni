9cb827be8451c39479cda763117800a77bf9823d
KirilNS <kirilnstoikov@gmail.com>
2019-11-01 21:53:50 +0200

    P07.Done

--- C#/EntityFrameworkCore/EntityFrameworkIntro/EF-Intro/SoftUni/StartUp.cs
+++ C#/EntityFrameworkCore/EntityFrameworkIntro/EF-Intro/SoftUni/StartUp.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System;
 using System.Text;
+using System.Globalization;
 
 namespace SoftUni
 {
@@ -9,17 +10,62 @@ namespace SoftUni
     {
         public static void Main(string[] args)
         {
-            using (SoftUniContext context=new SoftUniContext())
+            using (SoftUniContext context = new SoftUniContext())
             {
-                var result = AddNewAddressToEmployee(context);
+                var result = GetEmployeesInPeriod(context);
 
                 Console.WriteLine(result);
             }
         }
 
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+
+            var employees = context.Employees
+                            .Where(e => e.EmployeesProjects
+                            .Any(p => p.Project.StartDate.Year >= 2001 && p.Project.StartDate.Year <= 2003))
+                            .Select(e => new
+                            {
+                                EmployeeName = e.FirstName + " " + e.LastName,
+                                ManagerName = e.Manager.FirstName + " " + e.Manager.LastName,
+                                Projects = e.EmployeesProjects
+                                .Select(p => new
+                                {
+                                    ProjectName = p.Project.Name,
+                                    StartDate = p.Project.StartDate,
+                                    EndDate = p.Project.EndDate
+                                })
+                                .ToList()
+                            })
+                            .Take(10)
+                            .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.EmployeeName} - Manager: {employee.ManagerName}");
+
+                foreach (var project in employee.Projects)
+                {
+                    var startDate = project.StartDate
+                        .ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture);
+
+                    var endDate = project.EndDate == null ?
+                        "not finished" :
+                        project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture);
+
+                    sb.AppendLine($"--{project.ProjectName} - {startDate} - {endDate}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
         public static string AddNewAddressToEmployee(SoftUniContext context)
         {
-            var address = new Models.Address {AddressText= "Vitoshka 15",TownId=4 };
+            var address = new Models.Address { AddressText = "Vitoshka 15", TownId = 4 };
             var employee = context.Employees.First(e => e.LastName == "Nakov");
             employee.Address = address;
             context.SaveChanges();
@@ -38,7 +84,7 @@ namespace SoftUni
             }
 
             return sb.ToString().TrimEnd();
-            
+
         }
 
         public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
@@ -46,8 +92,8 @@ namespace SoftUni
             var employees = context.Employees
                 .Where(e => e.Department.Name == "Research and Development")
                 .Select(e => new { e.FirstName, e.LastName, e.Department.Name, e.Salary })
-                .OrderBy(e=>e.Salary)
-                .ThenByDescending(e=>e.FirstName)
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
                 .ToList();
 
             StringBuilder sb = new StringBuilder();
