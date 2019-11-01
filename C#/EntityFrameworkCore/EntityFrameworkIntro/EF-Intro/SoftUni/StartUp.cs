1210934d27eedf8f11c2bbf2531dd653510c1fc2
KirilNS <kirilnstoikov@gmail.com>
2019-11-01 18:48:29 +0200

    P06.Done

--- C#/EntityFrameworkCore/EntityFrameworkIntro/EF-Intro/SoftUni/StartUp.cs
+++ C#/EntityFrameworkCore/EntityFrameworkIntro/EF-Intro/SoftUni/StartUp.cs
@@ -11,10 +11,53 @@ namespace SoftUni
         {
             using (SoftUniContext context=new SoftUniContext())
             {
-                var result=GetEmployeesWithSalaryOver50000(context);
+                var result = AddNewAddressToEmployee(context);
 
                 Console.WriteLine(result);
             }
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            var address = new Models.Address {AddressText= "Vitoshka 15",TownId=4 };
+            var employee = context.Employees.First(e => e.LastName == "Nakov");
+            employee.Address = address;
+            context.SaveChanges();
+
+            var addresses = context.Employees
+                .Select(e => new { e.Address.AddressId, e.Address.AddressText })
+                .OrderByDescending(a => a.AddressId)
+                .Take(10)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var adr in addresses)
+            {
+                sb.AppendLine(adr.AddressText);
+            }
+
+            return sb.ToString().TrimEnd();
+            
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new { e.FirstName, e.LastName, e.Department.Name, e.Salary })
+                .OrderBy(e=>e.Salary)
+                .ThenByDescending(e=>e.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.FirstName} {emp.LastName} from {emp.Name} - ${emp.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
