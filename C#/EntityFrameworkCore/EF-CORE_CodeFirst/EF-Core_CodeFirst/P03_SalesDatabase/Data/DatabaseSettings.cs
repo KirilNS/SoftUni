ca666253b3a6e944017dadcd38914ed8646358b5
KirilNS <kirilnstoikov@gmail.com>
2019-11-03 23:52:14 +0200

    P03.Done

---
+++ C#/EntityFrameworkCore/EF-CORE_CodeFirst/EF-Core_CodeFirst/P03_SalesDatabase/Data/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    public static class DatabaseSettings
+    {
+        public const string connectionString = "Server=DESKTOP-FB04SD9\\SQLEXPRESS;" +
+            "Database=Sales;Integrated Security=true;";
+    }
+}
---
+++ C#/EntityFrameworkCore/EF-CORE_CodeFirst/EF-Core_CodeFirst/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,64 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+
+namespace P03_SalesDatabase.Data
+    
+{
+    public class SalesContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Store> Stores { get; set; }
+
+        protected  override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DatabaseSettings.connectionString);
+            }
+        }
+
+        protected  override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .Property(p => p.Name)
+                .HasMaxLength(100)
+                .IsUnicode(true)
+                .IsRequired();
+
+            modelBuilder.Entity<Customer>()
+                .Property(p => p.Email)
+                .HasMaxLength(80)
+                .IsUnicode(false)
+                .IsRequired();
+            modelBuilder.Entity<Customer>()
+                .Property(p => p.CreditCardNumber)
+                .IsRequired();
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsRequired()
+                .IsUnicode();
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .IsRequired();
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Quantity)
+                .IsRequired();
+
+            modelBuilder.Entity<Store>()
+                .Property(s => s.Name)
+                .HasMaxLength(80)
+                .IsUnicode()
+                .IsRequired();
+
+            modelBuilder.Entity<Sale>()
+                .Property(s => s.Date)
+                .IsRequired();
+                
+        }
+    }
+}
