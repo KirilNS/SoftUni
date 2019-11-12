47ef1589a9933cc33550879dea01b3c48182591c
KirilNS <kirilnstoikov@gmail.com>
2019-11-12 22:14:00 +0200

    Models Done

---
+++ C#/EntityFrameworkCore/AdvancedQuerying/AdvancedQuerying/BookShop/Data/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using BookShop.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Data.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Copies { get; set; }
+        public decimal Price { get; set; }
+        public EditionType EditionType { get; set; }
+        public AgeRestriction AgeRestriction { get; set; }
+        public Author Author { get; set; }
+        public ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
---
+++ C#/EntityFrameworkCore/AdvancedQuerying/AdvancedQuerying/BookShop/Data/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Data.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string  Name { get; set; }
+        public ICollection<BookCategory> CategoryBooks { get; set; }
+    }
+}
