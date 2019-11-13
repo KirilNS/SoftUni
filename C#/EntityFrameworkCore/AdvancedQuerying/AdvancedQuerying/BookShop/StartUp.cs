5c0ff402ded2479ff8f1bf16a9ebd9af3c5ca194
KirilNS <kirilnstoikov@gmail.com>
2019-11-13 21:00:29 +0200

    P01.Done

--- C#/EntityFrameworkCore/AdvancedQuerying/AdvancedQuerying/BookShop/StartUp.cs
+++ C#/EntityFrameworkCore/AdvancedQuerying/AdvancedQuerying/BookShop/StartUp.cs
@@ -1,6 +1,9 @@
 ﻿using BookShop.Data;
+using BookShop.Data.Models.Enums;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
+using System.Text;
 
 namespace BookShop
 {
@@ -10,9 +13,31 @@ namespace BookShop
         {
             using (var db=new BookShopContext())
             {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
+                string command = Console.ReadLine();
+
+                var result = GetBooksByAgeRestriction(db, command);
+
+                Console.WriteLine(result);
+                
             }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var age = Enum.Parse<AgeRestriction>(command, true);
+            var bookTitle = context.Books.Where(x => x.AgeRestriction == age)
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in bookTitle)
+            {
+                sb.AppendLine($"{item}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
