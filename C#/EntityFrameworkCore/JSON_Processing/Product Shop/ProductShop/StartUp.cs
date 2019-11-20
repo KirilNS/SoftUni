22db2558f5d7fe90bc3c5d468b07df4f0f796255
KirilNS <kirilnstoikov@gmail.com>
2019-11-20 17:20:51 +0200

    P01.Done

---
+++ C#/EntityFrameworkCore/JSON_Processing/Product Shop/ProductShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            using (var db = new ProductShopContext())
+            {
+                var inputJson = File.ReadAllText("./../../../Datasets/users.json");
+
+                var result = ImportUsers(db, inputJson);
+
+                Console.WriteLine(result);
+            }
+
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+    }
+}
