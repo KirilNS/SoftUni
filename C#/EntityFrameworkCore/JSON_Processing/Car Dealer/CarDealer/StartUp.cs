077bd69efc9d0d23b79d9cf03fb91108cc7403b3
KirilNS <kirilnstoikov@gmail.com>
2019-11-22 19:02:25 +0200

    P05.Done

---
+++ C#/EntityFrameworkCore/JSON_Processing/Car Dealer/CarDealer/StartUp.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db=new CarDealerContext())
+            {
+
+                var sales = File.ReadAllText($"./../../../Datasets/sales.json");
+
+                string result = ImportSales(db, sales);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers=JsonConvert.DeserializeObject<Supplier[]>(inputJson);
+
+            context.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}.";
+        }
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var parts = JsonConvert.DeserializeObject<Part[]>(inputJson)
+                .Where(x=>x.SupplierId<=31).ToArray();
+
+            context.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Length}.";
+        }
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var json = JsonConvert.DeserializeObject<CarDto[]>(inputJson);
+
+            foreach (var carDto in json)
+            {
+                Car car = new Car
+                {
+                    Make = carDto.Make,
+                    Model = carDto.Model,
+                    TravelledDistance = carDto.TravelledDistance
+                };
+
+                context.Cars.Add(car);
+
+                foreach (var partId in carDto.PartsId)
+                {
+                    PartCar partCar = new PartCar
+                    {
+                        CarId = car.Id,
+                        PartId = partId
+                    };
+
+                    if (car.PartCars.FirstOrDefault(p => p.PartId == partId) == null)
+                    {
+                        context.PartCars.Add(partCar);
+                    }
+                }
+            }
+
+            context.SaveChanges();
+
+            return $"Successfully imported {json.Count()}.";
+        }
+        public static string ImportCustomers(CarDealerContext context, string inputJson)
+        {
+            var customers = JsonConvert.DeserializeObject<Customer[]>(inputJson);
+
+            context.AddRange(customers);
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Length}.";
+        }
+        public static string ImportSales(CarDealerContext context, string inputJson)
+        {
+            var sales = JsonConvert.DeserializeObject<Sale[]>(inputJson);
+
+            context.AddRange(sales);
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Length}.";
+        }
+    }
+}
