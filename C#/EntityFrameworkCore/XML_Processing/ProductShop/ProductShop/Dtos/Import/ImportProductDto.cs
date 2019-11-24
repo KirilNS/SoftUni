92006c804ce9c5e6dd2e9a59703705d6131924e9
KirilNS <kirilnstoikov@gmail.com>
2019-11-24 21:21:36 +0200

    P01.Done

---
+++ C#/EntityFrameworkCore/XML_Processing/ProductShop/ProductShop/Dtos/Import/ImportProductDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ProductShop.Dtos.Import
+{
+    []
+    public class ImportProductDto
+    {
+        //    <Products>
+        //<Product>
+        //    <name>Care One Hemorrhoidal</name>
+        //    <price>932.18</price>
+        //    <sellerId>25</sellerId>
+        //    <buyerId>24</buyerId>
+        //</Product>
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int SellerId { get; set; }
+        public int BuyerId { get; set; }
+    }
+}
---
+++ C#/EntityFrameworkCore/XML_Processing/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new ProductShopContext())
+            {
+                var xmlToImport = File.ReadAllText($"../../../Datasets/users.xml");
+
+                Console.WriteLine(ImportUsers(db, xmlToImport)); 
+            }
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportUserDto[]), new XmlRootAttribute("Users"));
+
+            var users=(ImportUserDto[])serializer.Deserialize(new StringReader(inputXml));
+
+            foreach (var user in users)
+            {
+                User userToImport = new User
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Age = user.Age
+                };
+
+                context.Add(userToImport);
+            }
+
+            int result=context.SaveChanges();
+
+            return $"Successfully imported {result}";
+        }
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+
+        }
+    }
+}
