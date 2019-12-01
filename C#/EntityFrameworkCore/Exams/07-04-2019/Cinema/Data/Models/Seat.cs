f8639a2680df4743f7b2108bff3e500cbab6c338
KirilNS <kirilnstoikov@gmail.com>
2019-12-01 13:40:11 +0200

    P01.Done

---
+++ C#/EntityFrameworkCore/Exams/07-04-2019/Cinema/Data/Models/Seat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema.Data.Models
+{
+    public class Seat
+    {
+        //Id – integer, Primary Key
+        // – integer, foreign key(required)
+        //Hall – the seaHallIdt’s hall
+        public int Id { get; set; }
+        public int HallId { get; set; }
+        public Hall Hall { get; set; }
+    }
+}
