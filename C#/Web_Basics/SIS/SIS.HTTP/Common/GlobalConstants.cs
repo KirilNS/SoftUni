11bbc255d93dbb95a2d4bca504e04cdb5c1521d9
KirilNS <kirilnstoikov@gmail.com>
2020-01-01 14:46:28 +0200

    Request Done

--- C#/Web_Basics/SIS/SIS.HTTP/Common/GlobalConstants.cs
+++ C#/Web_Basics/SIS/SIS.HTTP/Common/GlobalConstants.cs
@@ -7,5 +7,7 @@
         public const string HostHeaderKey = "HOST";
 
         public const string HttpNewLine = "\r\n";
+
+        public const string UnsuportedMethodExceptionMessage = "The method {0} is not supported!";
     }
 }
--- C#/Web_Basics/SIS/SIS.HTTP/Headers/HttpHeaderCollection.cs
+++ C#/Web_Basics/SIS/SIS.HTTP/Headers/HttpHeaderCollection.cs
@@ -1,10 +1,51 @@
-﻿using System;
+﻿using SIS.HTTP.Common;
+using SIS.HTTP.Headers.Contracts;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SIS.HTTP.Headers
 {
-    class HttpHeaderCollection
+    public class HttpHeaderCollection : IHttpHeaderCollection
     {
+        private readonly Dictionary<string,HttpHeader> headers;
+
+        public HttpHeaderCollection()
+        {
+            this.headers = new Dictionary<string, HttpHeader>();
+        }
+        public void AddHeader(HttpHeader header)
+        {
+            CoreValidator.ThrowIfNull(header, nameof(header));
+
+            this.headers.Add(header.Key, header);
+        }
+
+        public bool ContainsHeader(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.headers.ContainsKey(key);
+        }
+
+        public HttpHeader GetHeader(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.headers[key];
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in this.headers)
+            {
+                sb.AppendLine(item.Value.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
