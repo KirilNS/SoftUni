8e56c4674844874f558cb75f0c0ebca7522d86f7
KirilNS <kirilnstoikov@gmail.com>
2020-01-03 21:08:38 +0200

    P05.Done

---
+++ C#/TechModule/IntroAndBasicSyntax/BasicSyntax/P03.Vacation/StartUp.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPeople = int.Parse(Console.ReadLine());
+            string typeOfPeople = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+
+            double sum = 0.0;
+
+            switch (typeOfPeople)
+            {
+                case "Students" when numberPeople >= 30:
+                {
+                    switch (dayOfWeek)
+                    {
+                        case "Friday":
+                            sum = (numberPeople * 8.45) * 0.85;
+                            break;
+                        case "Saturday":
+                            sum = (numberPeople * 9.8) * 0.85;
+                            break;
+                        case "Sunday":
+                            sum = (numberPeople * 10.46) * 0.85;
+                            break;
+                    }
+
+                    break;
+                }
+                case "Students" when numberPeople < 30:
+                {
+                    switch (dayOfWeek)
+                    {
+                        case "Friday":
+                            sum = (numberPeople * 8.45);
+                            break;
+                        case "Saturday":
+                            sum = (numberPeople * 9.8);
+                            break;
+                        case "Sunday":
+                            sum = (numberPeople * 10.46);
+                            break;
+                    }
+
+                    break;
+                }
+                case "Business" when numberPeople >= 100:
+                {
+                    numberPeople -= 10;
+                    if (dayOfWeek == "Friday")
+                    {
+                        sum = numberPeople * 10.9;
+                    }
+                    else if (dayOfWeek == "Saturday")
+                    {
+                        sum = numberPeople * 15.6;
+                    }
+                    else if (dayOfWeek == "Sunday")
+                    {
+                        sum = numberPeople * 16.0;
+                    }
+
+                    break;
+                }
+                case "Business" when dayOfWeek == "Friday":
+                    sum = numberPeople * 10.9;
+                    break;
+                case "Business" when dayOfWeek == "Saturday":
+                    sum = numberPeople * 15.6;
+                    break;
+                case "Business":
+                {
+                    if (dayOfWeek == "Sunday")
+                    {
+                        sum = numberPeople * 16.0;
+                    }
+
+                    break;
+                }
+                case "Regular" when numberPeople >= 10 && numberPeople <= 20:
+                {
+                    if (dayOfWeek == "Friday")
+                    {
+                        sum = (numberPeople * 15.0) * 0.95;
+                    }
+                    else if (dayOfWeek == "Saturday")
+                    {
+                        sum = (numberPeople * 20.0) * 0.95;
+                    }
+                    else if (dayOfWeek == "Sunday")
+                    {
+                        sum = (numberPeople * 22.5) * 0.95;
+                    }
+
+                    break;
+                }
+                case "Regular" when dayOfWeek == "Friday":
+                    sum = (numberPeople * 15.0);
+                    break;
+                case "Regular" when dayOfWeek == "Saturday":
+                    sum = (numberPeople * 20.0);
+                    break;
+                case "Regular":
+                {
+                    if (dayOfWeek == "Sunday")
+                    {
+                        sum = (numberPeople * 22.5);
+                    }
+
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Total price: {sum:F2}");
+        }
+    }
+}
---
+++ C#/TechModule/IntroAndBasicSyntax/BasicSyntax/P05.Login/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace P05.Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+
+            for (int i = 0; i < 4; i++)
+            {
+                string input = Console.ReadLine();
+
+                string password=String.Empty;
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    password+= input[input.Length-j-1];
+                }
+
+                if (password == username)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    return;
+                }
+                else if (i<3)
+                {
+                    Console.WriteLine($"Incorrect password. Try again.");
+                }
+
+                
+            }
+
+            Console.WriteLine($"User {username} blocked!");
+        }
+    }
+}
