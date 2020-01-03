6e5441018158a7ca79234b462989f23a185fa84d
KirilNS <kirilnstoikov@gmail.com>
2020-01-03 22:15:08 +0200

    P08.Done

---
+++ C#/TechModule/IntroAndBasicSyntax/BasicSyntax/P08.TriangleOfNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P08.TriangleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < row+1; col++)
+                {
+                    Console.Write(row+1+" ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
