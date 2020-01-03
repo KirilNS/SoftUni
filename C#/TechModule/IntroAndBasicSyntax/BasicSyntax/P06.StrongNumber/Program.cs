945da526354193dadfc753d1cc9e0fefd61f9cd5
KirilNS <kirilnstoikov@gmail.com>
2020-01-03 22:14:24 +0200

    P06.Done

---
+++ C#/TechModule/IntroAndBasicSyntax/BasicSyntax/P06.StrongNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P06.StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            var strongNumber = input;
+            int sum = 0;
+
+            while (input != 0)
+            {
+                var lastNumber = input % 10;
+                input = input / 10;
+
+                var number = 1;
+
+                for (int i = 1; i <= lastNumber; i++)
+                {
+                    number *= i;
+                }
+
+                sum += number;
+            }
+
+            if (sum == strongNumber)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
