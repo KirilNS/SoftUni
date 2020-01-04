78548c44c137d55aa4e81f0082cbc7fe81bc6fa0
KirilNS <kirilnstoikov@gmail.com>
2020-01-04 15:52:55 +0200

    All Done

---
+++ C#/TechModule/IntroAndBasicSyntax/BasicSyntax/P10.RageExpenses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P10.RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGameCount = int.Parse(Console.ReadLine());
+            decimal headsetPrice = decimal.Parse(Console.ReadLine());
+            decimal mousePrice = decimal.Parse(Console.ReadLine());
+            decimal keyboardPrice = decimal.Parse(Console.ReadLine());
+            decimal displayPrice = decimal.Parse(Console.ReadLine());
+
+            decimal totalPrice = (lostGameCount / 2 * headsetPrice) + (lostGameCount / 3 * mousePrice) +
+                                 (lostGameCount / 6 * keyboardPrice)
+                                 + (lostGameCount / 12 * displayPrice);
+
+            Console.WriteLine($"Rage expenses: {totalPrice:F2} lv.");
+        }
+    }
+}
