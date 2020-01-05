7005e8c2c6b05ae43a9accd00469fe747fb07ee9
KirilNS <kirilnstoikov@gmail.com>
2020-01-05 14:19:57 +0200

    All DOne

---
+++ C#/TechModule/DataTypes/DataTypesAndVar/P10.Pokemon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P10.Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            double half = n / 2.0;
+            int count = 0;
+
+            while (n >= m && n != 0)
+            {
+
+                n -= m;
+                count++;
+
+                if (n == half)
+                {
+                    if (y != 0)
+                    {
+                        n /= y;
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine(n);
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ C#/TechModule/DataTypes/DataTypesAndVar/P11.Snowballs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace P11.Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSnowballs = int.Parse(Console.ReadLine());
+
+            BigInteger biggestValue = 0;
+            int bigSnowballSnow=0;
+            int bigSnowballTime=0;
+            int bigSnowballQuality = 0;
+
+            for (int i = 0; i < numberOfSnowballs; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger value = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+                if (biggestValue <= value)
+                {
+                    biggestValue = value;
+                    bigSnowballSnow = snowballSnow;
+                    bigSnowballTime = snowballTime;
+                    bigSnowballQuality = snowballQuality;
+                }
+            }
+
+            Console.WriteLine($"{bigSnowballSnow} : {bigSnowballTime} = {biggestValue} ({bigSnowballQuality})");
+        }
+    }
+}
