1f453d293d34336ee6444f4d3625f7573d466585
KirilNS <kirilnstoikov@gmail.com>
2020-01-05 20:39:06 +0200

    P02.Done

---
+++ C#/TechModule/Arrays/Arrays/P01.Train/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+
+            int[] wagonsCapacity=new int[wagons];
+
+            int sum = 0;
+
+            for (int i = 0; i < wagons; i++)
+            {
+                int numberOfPeople = int.Parse(Console.ReadLine());
+
+                wagonsCapacity[i] = numberOfPeople;
+                sum += numberOfPeople;
+            }
+
+            Console.WriteLine(string.Join(" ",wagonsCapacity));
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C#/TechModule/Arrays/Arrays/P02.CommonElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P02.CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            string[] secondArray = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var VARIABLE in secondArray)
+            {
+                foreach (var item in firstArray)
+                {
+                    if (item == VARIABLE)
+                    {
+                        Console.Write(item+" ");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
