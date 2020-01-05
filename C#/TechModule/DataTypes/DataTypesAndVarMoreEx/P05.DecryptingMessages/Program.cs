3123f4bc64a6864720cdee20997b9b7f08b52ac0
KirilNS <kirilnstoikov@gmail.com>
2020-01-05 16:59:38 +0200

    All Done

---
+++ C#/TechModule/DataTypes/DataTypesAndVarMoreEx/P05.DecryptingMessages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P05.DecryptingMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+
+            int lines = int.Parse(Console.ReadLine());
+
+            string message=String.Empty;
+
+            for (int i = 0; i < lines; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+
+                message += (char)(letter + key);
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
