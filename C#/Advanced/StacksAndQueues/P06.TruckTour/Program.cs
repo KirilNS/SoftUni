a70187e96b0a18fcba1b0ea72a3cd6a50c6041f5
KirilNS <kirilnstoikov@gmail.com>
2020-01-12 14:37:07 +0200

    P06.DOne

---
+++ C#/Advanced/StacksAndQueues/P06.TruckTour/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class TruckTour
+{
+    static void Main()
+    {
+        long stops = long.Parse(Console.ReadLine());
+        Queue<string> myQueue = new Queue<string>();
+
+        for (long i = 0; i < stops; i++)
+        {
+            myQueue.Enqueue(Console.ReadLine());
+        }
+
+        bool canMakeIt = false;
+
+
+        for (long i = 0; i < stops; i++)
+        {
+            long fuel = 0;
+            foreach (var item in myQueue)
+            {
+                fuel += item.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray()[0];
+                fuel -= item.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray()[1];
+                if (fuel < 0)
+                {
+                    canMakeIt = false;
+                    break;
+                }
+                else
+                {
+                    canMakeIt = true;
+                }
+            }
+            if (canMakeIt)
+            {
+                Console.WriteLine(i);
+                return;
+            }
+            string helper = myQueue.Dequeue();
+            myQueue.Enqueue(helper);
+
+        }
+    }
+}
