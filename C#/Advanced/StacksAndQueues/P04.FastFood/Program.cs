f65f09a36d99708f44a4264743ee17c56426505b
KirilNS <kirilnstoikov@gmail.com>
2020-01-12 14:11:24 +0200

    P06.Done

---
+++ C#/Advanced/StacksAndQueues/P04.FastFood/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+            int[] input=Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Queue<int> orders=new Queue<int>(input);
+
+            int biggestOrder = orders.Max();
+
+            while (orders.Count!=0)
+            {
+                int order = orders.Peek();
+
+                bool isBigger = quantity - order >= 0;
+
+                if (!isBigger)
+                {
+                    break;
+                }
+
+                order = orders.Dequeue();
+                quantity -= order;
+            }
+
+            Console.WriteLine(biggestOrder);
+
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+
+
+        }
+    }
+}
---
+++ C#/Advanced/StacksAndQueues/P05.FashionBotique/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace P05.FashionBotique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] boxWithClothes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int input = int.Parse(Console.ReadLine());
+
+            Stack<int> clothes=new Stack<int>(boxWithClothes);
+
+            int capacity = input;
+            int boxCount = 1;
+
+            while (clothes.Count!=0)
+            {
+                
+                int cloth=clothes.Peek();
+
+                if (capacity >= cloth)
+                {
+                    clothes.Pop();
+                    capacity -= cloth;
+                }
+                else
+                {
+                    capacity = input;
+                    boxCount++;
+                }
+            }
+
+            Console.WriteLine(boxCount);
+
+
+        }
+    }
+}
